22c213e3a84dd8d222d49869f10c51bdf5d41243
sebastian <sebmuell@phys.ethz.ch>
2017-01-13 14:54:23 +0100

    make the 'finger plot' histogram

--- photon_stream/fact/__init__.py
+++ photon_stream/fact/__init__.py
@@ -1,4 +1,7 @@
 from .Geometry import Geometry
 from .Event import Event
 from .Run import Run
+from .Run import JsonLRun
 from .PhotonCluster import PhotonCluster
+from .PhotonCluster import PhotonClusterOnTimeSeries
+from . import finger_histogram
---
+++ photon_stream/fact/finger_histogram.py
@@ -0,0 +1,67 @@
+import numpy as np
+from .PhotonCluster import PhotonClusterOnTimeSeries
+from .Run import JsonLRun
+import tqdm
+import json
+
+def finger_histogram(time_series, max_multiplicity=20):
+    counts = np.zeros(max_multiplicity, dtype=np.int64)
+
+    if len(time_series) < 1:
+        return counts
+
+    clusters = PhotonClusterOnTimeSeries(time_series)
+    # single photons
+    counts[0] = (clusters.labels == -1).sum()
+
+    for label in range(clusters.number):
+        number_of_photons_in_cluster = (clusters.labels == label).sum()
+
+        if number_of_photons_in_cluster <= max_multiplicity:
+            counts[number_of_photons_in_cluster - 1] += 1
+        else:
+            print('Warning, found cluster of', number_of_photons_in_cluster, 'p.e, but max_multiplicity is only ', max_multiplicity)
+    return counts
+    
+
+def finger_histogram_of_event(event, max_multiplicity=20):
+    counts = np.zeros(max_multiplicity, dtype=np.int64)
+    valid_time_lines = 0
+    for time_line in event.photon_stream.time_lines:
+
+        tl_counts = finger_histogram(time_line, max_multiplicity)
+
+        if tl_counts[0] > 16 or tl_counts[1] > 8 or tl_counts[2] > 4 or tl_counts[3] > 2 or tl_counts[4] > 1:
+            #print('Warning, time series is spooky', tl_counts)
+            continue
+        else:
+            counts += tl_counts
+            valid_time_lines += 1
+
+    return {'counts': counts, 'valid_time_lines': valid_time_lines}
+
+
+def finger_event(event, max_multiplicity=20):
+    finger_event = {}
+    finger_event['UnixTimeUTC'] = event.time.timestamp()
+    finger_event['RUNID'] = event.run_number
+    finger_event['EventNum'] = event.number
+    finger_event['NIGHT'] = event.night
+    
+    fhist = finger_histogram_of_event(
+        event=event,
+        max_multiplicity=max_multiplicity)
+
+    finger_event['1pe_multiplicity'] = fhist['counts'].tolist()
+    finger_event['valid_pixels'] = fhist['valid_time_lines']
+    return finger_event
+
+
+def make_run_1pe_multiplicity(run, output_path, max_multiplicity=20):
+    with open(output_path, 'w') as f:
+        for event in tqdm.tqdm(run):
+            f_evt = finger_event(event, max_multiplicity)
+            f_evt_json = json.dumps(f_evt)
+            f.write(f_evt_json+'\n')
+        
+
