aa01f3c48360341965b7b15fecf9eec12e978a00
Dominik Neise <neised@phys.ethz.ch>
2017-01-20 17:33:50 +0100

    make PhotonStream.number_photons a public property

--- photon_stream/PhotonStream.py
+++ photon_stream/PhotonStream.py
@@ -5,11 +5,9 @@ class PhotonStream(object):
         self.slice_duration = 0.0
         self.time_lines = []
 
-    def _number_photons(self):
-        number_photons = 0
-        for time_line in self.time_lines:
-            number_photons += len(time_line)
-        return number_photons
+    @property
+    def number_photons(self):
+        return sum(len(tl) for tl in self.time_lines)
 
     def _min_max_arrival_slice(self):
         max_slice = 0
@@ -23,22 +21,22 @@ class PhotonStream(object):
                 if min_slice_on_current_time_line < min_slice:
                     min_slice = min_slice_on_current_time_line
 
-        return min_slice, max_slice      
+        return min_slice, max_slice
 
     def truncate_time_lines(self, start_time, end_time):
         trunc_ps = PhotonStream()
         trunc_ps.slice_duration = self.slice_duration
         trunc_ps.time_lines = truncate_time_lines(
-            time_lines=self.time_lines, 
-            slice_duration=self.slice_duration, 
-            start_time=start_time, 
+            time_lines=self.time_lines,
+            slice_duration=self.slice_duration,
+            start_time=start_time,
             end_time=end_time)
         return trunc_ps
 
     def __repr__(self):
         info = 'PhotonStream('
         info+= str(len(self.time_lines))+' time lines, '
-        info+= str(self._number_photons())+' photons'
+        info+= str(self.number_photons)+' photons'
         info+= ')'
         return info
 
@@ -51,6 +49,6 @@ def truncate_time_lines(time_lines, slice_duration, start_time, end_time):
             arrival_time = arrival_slice*slice_duration
             if arrival_time >= start_time and arrival_time < end_time:
                 trunc_time_line.append(arrival_slice)
-        trunc_time_lines.append(trunc_time_line)       
+        trunc_time_lines.append(trunc_time_line)
 
     return trunc_time_lines
--- photon_stream/experimental/_input_output.py
+++ photon_stream/experimental/_input_output.py
@@ -3,13 +3,13 @@ from ..PhotonStream import PhotonStream
 
 
 def append_photonstream_to_binary_file(photonstream, file_handle):
-    
+
     # WRITE NUMBER OF TIMELINES
     number_of_time_lines = np.uint64(len(photonstream.time_lines))
     file_handle.write(number_of_time_lines.tobytes())
 
     # WRITE NUMBER OF PHOTONS
-    number_of_photons = np.uint64(photonstream._number_photons())
+    number_of_photons = np.uint64(photonstream.number_photons)
     file_handle.write(number_of_photons.tobytes())
 
     # WRITE SLICE DURATION
@@ -21,7 +21,7 @@ def append_photonstream_to_binary_file(photonstream, file_handle):
     if max_slice < np.iinfo(np.uint8).max:
         dtype = np.uint8
     elif max_slice < np.iinfo(np.uint16).max:
-        dtype = np.uint16      
+        dtype = np.uint16
     elif max_slice < np.iinfo(np.uint32).max:
         dtype = np.uint32
     elif max_slice < np.iinfo(np.uint64).max:
@@ -33,7 +33,7 @@ def append_photonstream_to_binary_file(photonstream, file_handle):
     slice_width = np.uint8(dtype().itemsize)
     file_handle.write(slice_width.tobytes())
 
-    # WRITE PHOTON ARRIVAL SLICES 
+    # WRITE PHOTON ARRIVAL SLICES
     linebreak = np.array([np.iinfo(dtype).max], dtype=dtype)
     for pixel_arrivals in photonstream.time_lines:
         arrivals = np.array(pixel_arrivals, dtype=dtype)
@@ -63,7 +63,7 @@ def read_photonstream_from_binary_file(file_handle):
     elif slice_width == np.uint16().itemsize:
         dtype = np.uint16()
     elif slice_width == np.uint32().itemsize:
-        dtype = np.uint32()        
+        dtype = np.uint32()
     elif slice_width == np.uint64().itemsize:
         dtype = np.uint64()
     else:
@@ -71,8 +71,8 @@ def read_photonstream_from_binary_file(file_handle):
 
     # READ PHOTONSTREAM
     photn_stream = np.fromfile(
-        file_handle, 
-        dtype=dtype, 
+        file_handle,
+        dtype=dtype,
         count=number_of_photons+number_of_time_lines)
 
     linebreak = np.array([np.iinfo(dtype).max], dtype=dtype)
