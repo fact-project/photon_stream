1ec6c71f39be774f616bec54c8c89960459c0c35
Dominik Neise <neised@phys.ethz.ch>
2017-01-25 09:35:06 +0100

    remove class PhotonStreamCluster; went into PhotonStream

--- photon_stream/PhotonCluster.py
+++ photon_stream/PhotonCluster.py
@@ -1,33 +1,5 @@
-from photon_stream import Geometry
 from sklearn.cluster import DBSCAN
 import numpy as np
-
-geo = Geometry()
-
-class PhotonStreamCluster(object):
-    def __init__(self, event, eps=0.1, min_samples=20, deg_over_s=0.35e9):
-
-        xyt = event.photon_stream.flatten()
-
-        if xyt.shape[0] == 0:
-            self.labels = np.array([])
-            self.number = 0
-            return
-
-        xyt[:,0:2] /= (geo.fov_radius*2.0)
-        xyt[:,2] /= (geo.fov_radius*2.0)/deg_over_s
-
-        dbscan = DBSCAN(eps=eps, min_samples=min_samples).fit(xyt)
-        self.labels = dbscan.labels_
-
-        # Number of clusters in labels, ignoring noise if present.
-        self.number = len(set(self.labels)) - (1 if -1 in self.labels else 0)
-
-    def __repr__(self):
-        out = 'PhotonStreamCluster('
-        out += 'number of clusters '+str(self.number)
-        out += ')\n'
-        return out
 
 
 class PhotonTimeSeriesCluster(object):
@@ -39,7 +11,7 @@ class PhotonTimeSeriesCluster(object):
             return
 
         time_series_array = np.array(time_series)
-        time_series_array_rs = time_series_array.reshape(-1,1)
+        time_series_array_rs = time_series_array.reshape(-1, 1)
 
         dbscan = DBSCAN(eps=eps, min_samples=2)
         dbfit = dbscan.fit(time_series_array_rs)
