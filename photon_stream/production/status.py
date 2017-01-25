a4d71241e5052ae14b2fdcc591de3e8a3eb97b9a
Sebastian Mueller <relleums@ihp-pc42>
2017-01-25 21:34:28 +0100

    finish imports

--- photon_stream/production/status.py
+++ photon_stream/production/status.py
@@ -1,12 +1,15 @@
 import os
 from tqdm import tqdm
+import pandas as pd
+import numpy as np
 
 from . import runinfo
 from . import tools
 
 
 def status(photon_stream_dir, known_runs_database='known_runs.msg'):
-    info_path = os.path.join(photon_stream_dir, known_runs_database)
+    info_path = os.path.abspath(
+	os.path.join(photon_stream_dir, known_runs_database))
     try:
         info = runinfo.read_runinfo_from_file(info_path)
     except:
@@ -25,7 +28,7 @@ def status(photon_stream_dir, known_runs_database='known_runs.msg'):
         night = info['fNight'][index]
         run = info['fRunID'][index]
 
-        if info['fRunTypeKey'][index] == observation_key:
+        if info['fRunTypeKey'][index] == runinfo.observation_key:
             if info['photon_stream_NumTrigger'][index]==0 and not info['photon_stream_exists'][index]:
                 file_name = '{yyyymmnn:08d}_{rrr:03d}.phs.jsonl.gz'.format(
                     yyyymmnn=night,
@@ -39,8 +42,8 @@ def status(photon_stream_dir, known_runs_database='known_runs.msg'):
                     file_name)
 
                 if os.path.exists(run_path):    
-                    info['photon_stream_exists'][index] = True
-                    info['photon_stream_NumTrigger'][index] = tools.number_of_events_in_run(run_path)
+                    info.set_value(index, 'photon_stream_exists', True) 
+                    info.set_value(index, 'photon_stream_NumTrigger', tools.number_of_events_in_run(run_path))
                     print('New run '+str(night)+' '+str(run)+' '+str(info['photon_stream_NumTrigger'][index])+' trigger.')
 
     runinfo.write_runinfo_to_file(info, info_path)
