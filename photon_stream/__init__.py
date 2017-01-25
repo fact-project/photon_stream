67458380dc9f883572934ca00bc992e4fc994ce6
Dominik Neise <neised@phys.ethz.ch>
2017-01-25 09:35:53 +0100

    adjust to moved PhotonStreamCluster and remove Geometry

--- photon_stream/__init__.py
+++ photon_stream/__init__.py
@@ -1,10 +1,5 @@
-
-from .Geometry import Geometry
 from .lowlevel import *
-
-from .PhotonCluster import PhotonStreamCluster
 from .PhotonCluster import PhotonTimeSeriesCluster
 from . import finger_histogram
-
 from . import production
 from . import experimental
