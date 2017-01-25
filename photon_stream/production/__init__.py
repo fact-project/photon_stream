d8729645fd5db711e75844d753d0e786620a1680
sebastian <sebmuell@phys.ethz.ch>
2017-01-25 16:09:07 +0100

    a tools to find out about existing runs

--- photon_stream/production/__init__.py
+++ photon_stream/production/__init__.py
@@ -2,3 +2,4 @@ from . import tools
 from .submit_to_qsub import submit_to_qsub
 from . import runinfo
 from .write_worker_script import write_worker_script
+from .status import status
---
+++ photon_stream/production/status.py
@@ -0,0 +1,46 @@
+import os
+from tqdm import tqdm
+
+from . import runinfo
+from . import tools
+
+
+def status(photon_stream_dir, known_runs_database='known_runs.msg'):
+    info_path = os.path.join(photon_stream_dir, known_runs_database)
+    try:
+        info = runinfo.read_runinfo_from_file(info_path):
+    except:
+        info = runinfo.download_latest_runinfo()
+    number_of_runs = len(info['fRunID'])
+
+    info['photon_stream_exists'] = pd.Series(
+        np.zeros(number_of_runs, dtype=np.bool), 
+        index=info.index)
+
+    info['photon_stream_NumTrigger'] = pd.Series(
+        np.zeros(number_of_runs, dtype=np.int), 
+        index=info.index)
+
+    for index, row in tqdm(info.iterrows()):
+        night = info['fNight'][index]
+        run = info['fRunID'][index]
+
+        if info['fRunTypeKey'][index] == observation_key:
+            if info['photon_stream_NumTrigger'][index]==0 and not info['photon_stream_exists'][index]:
+                file_name = '{yyyymmnn:08d}_{rrr:03d}.phs.jsonl.gz'.format(
+                    yyyymmnn=night,
+                    rrr=run)
+
+                run_path = os.path.join(
+                    photon_stream_dir, 
+                    '{yyyy:04d}'.format(yyyy=tools.night_id_2_yyyy(night)), 
+                    '{mm:02d}'.format(mm=tools.night_id_2_mm(night)), 
+                    '{nn:02d}'.format(nn=tools.night_id_2_nn(night)), 
+                    file_name)
+
+                if os.path.exists(run_path):    
+                    info['photon_stream_exists'][index] = True
+                    info['photon_stream_NumTrigger'][index] = tools.number_of_events_in_run(run_path)
+                    print('New run '+str(night)+' '+str(run)+' '+str(info['photon_stream_NumTrigger'][index])+' trigger.')
+
+    runinfo.write_runinfo_to_file(info, info_path)
