ee08bf5eb01514579edcee63ec43929bf7b740f0
sebastian <sebmuell@phys.ethz.ch>
2017-01-31 14:25:15 +0100

    remove obsolete

--- photon_stream/production/tools.py
+++ photon_stream/production/tools.py
@@ -15,32 +15,14 @@ def jobs_where_path_exists(jobs, path='raw_path'):
 def night_id_2_yyyy(night):
     return night // 10000
 
+
 def night_id_2_mm(night):
     return (night // 100) % 100
+
 
 def night_id_2_nn(night):
     return night % 100
 
-def write_json(path, dic):
-    with open(path, 'w') as job_out:
-        job_out.write(json.dumps(un_numpyify_dictionary(dic), indent=4))
-
-def un_numpyify_dictionary(dic):
-    ret = {}
-    for k, v in list(dic.items()):
-        if isinstance(v, dict):
-            ret[k] = un_numpyify_dictionary(v)
-        elif isinstance(v, np.ndarray):
-            if v.dtype == np.float32:
-                v = v.astype(np.float64)
-            ret[k] = v.tolist()
-        elif isinstance(v, np.floating):
-            ret[k] = float(v)
-        elif isinstance(v, np.integer):
-            ret[k] = int(v)
-        else:
-            ret[k] = v
-    return ret
 
 def number_of_events_in_run(run_path):
     ps = subprocess.Popen(['zcat', run_path], stdout=subprocess.PIPE)
