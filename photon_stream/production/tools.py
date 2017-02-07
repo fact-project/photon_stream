1c880dd1a040fa19d59401e0d9f4a490abb1c403
sebastian <sebmuell@phys.ethz.ch>
2017-02-07 15:45:17 +0100

    remove obsolete tool

--- photon_stream/production/tools.py
+++ photon_stream/production/tools.py
@@ -29,17 +29,4 @@ def number_of_events_in_run(run_path):
     wc_out = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
     ps.wait()
     return int(wc_out)
-
-
-def sparse_nights_in_range(start_nigth=20110101, end_nigth=20170301, gap_nights=27):
-    current_night = dt.datetime.strptime(str(start_nigth), '%Y%m%d')
-    nights = []
-    while True:
-        current_night += dt.timedelta(gap_nights)
-        night = int(current_night.strftime(format='%Y%m%d'))
-        if night > end_nigth:
-            break
-        print(night)
-        nights.append(night)
-    return nights
-        
+    
