69fdfd0de0a781b858d1da9ee22ea7a154d9a21f
sebastian <sebmuell@phys.ethz.ch>
2017-02-07 14:21:03 +0100

    A more general overview print

--- photon_stream/production/status.py
+++ photon_stream/production/status.py
@@ -50,34 +50,159 @@ def status(photon_stream_dir, known_runs_database='known_runs.msg'):
     runinfo.write_runinfo_to_file(info, info_path)
 
 
-def print_status_in_range(start_night, end_night, info):
+def runs_in_range_str(info, start_night, end_night, max_trigger_rate=200):
     past_start = info['fNight'] >= start_night
     before_end = info['fNight'] < end_night
     is_observation_run = info['fRunTypeKey'] == runinfo.observation_key
 
-    in_range = past_start*before_end*is_observation_run
+    rate_below_max_trigger_rate = (
+        info['fNumExt1Trigger'] + 
+        info['fNumExt2Trigger'] + 
+        info['fNumPhysicsTrigger'] + 
+        info['fNumPedestalTrigger']) < 300*max_trigger_rate # 300seconds per run
 
-    night_ids = info['fNight'][in_range]
-    run_ids = info['fRunID'][in_range]
+    has_at_least_one_expected_trigger = (
+        info['fNumExt1Trigger'] + 
+        info['fNumExt2Trigger'] + 
+        info['fNumPhysicsTrigger'] + 
+        info['fNumPedestalTrigger']) > 0
+
+    valid = (
+        past_start&
+        before_end&
+        is_observation_run&
+        rate_below_max_trigger_rate&
+        has_at_least_one_expected_trigger)
+
+    night_ids = info['fNight'][valid]
+    run_ids = info['fRunID'][valid]
     expected_triggers = (
-        info['fNumExt1Trigger'][in_range] + 
-        info['fNumExt2Trigger'][in_range] + 
-        info['fNumPhysicsTrigger'][in_range] + 
-        info['fNumPedestalTrigger'][in_range])
-    actual_triggers = info['photon_stream_NumTrigger'][in_range]
-    exisences = info['photon_stream_exists'][in_range]
+        info['fNumExt1Trigger'][valid] + 
+        info['fNumExt2Trigger'][valid] + 
+        info['fNumPhysicsTrigger'][valid] + 
+        info['fNumPedestalTrigger'][valid])
+    actual_triggers = info['photon_stream_NumTrigger'][valid]
+    exisences = info['photon_stream_exists'][valid]
     completation_ratios = actual_triggers/expected_triggers
 
-    print(' night  run  expected_events actualevents complete_ratio')
+    out =  ''
+    out += 'year month night run '+table_header_str()
+    out += '---------------------'+table_line_str()
     for i, run_id in enumerate(run_ids):
-        print('{night:08d} {rrr:03d} {expected_evts:>6d} {actual_evts:>6d} '.format(
-            night=night_ids.iloc[i],  
-            rrr=run_ids.iloc[i],
-            expected_evts=int(expected_triggers.iloc[i]),
-            actual_evts=int(actual_triggers.iloc[i]),
-            )+progress(completation_ratios.iloc[i]))
+        out += '{year:04d}   {month:02d}   {night:02d}   {run:03d} '.format(
+            year=tools.night_id_2_yyyy(night_ids.iloc[i]),
+            month=tools.night_id_2_mm(night_ids.iloc[i]),
+            night=tools.night_id_2_nn(night_ids.iloc[i]),
+            run=run_ids.iloc[i])
+        out += table_row_str(
+            expected_events=int(expected_triggers.iloc[i]),
+            actual_events=int(actual_triggers.iloc[i]))
+    return out
+
+
+def overview_str(info, max_trigger_rate=120):
+    is_obs = info['fRunTypeKey'] == runinfo.observation_key
+
+    rate_below_max_trigger_rate = (
+        info['fNumExt1Trigger'] + 
+        info['fNumExt2Trigger'] + 
+        info['fNumPhysicsTrigger'] + 
+        info['fNumPedestalTrigger']) < 300*max_trigger_rate # 300seconds per run
+
+    has_at_least_one_expected_trigger = (
+        info['fNumExt1Trigger'] + 
+        info['fNumExt2Trigger'] + 
+        info['fNumPhysicsTrigger'] + 
+        info['fNumPedestalTrigger']) > 0
+
+    valid = is_obs&rate_below_max_trigger_rate&has_at_least_one_expected_trigger
+
+    expected_triggers = (
+        info['fNumExt1Trigger'][valid] + 
+        info['fNumExt2Trigger'][valid] + 
+        info['fNumPhysicsTrigger'][valid] + 
+        info['fNumPedestalTrigger'][valid])
+    actual_triggers = info['photon_stream_NumTrigger'][valid]
+
+
+    total_expected_events = int(expected_triggers.sum())
+    total_actual_events = int(actual_triggers.sum())
+    # full overview
+    out = ''
+    out += 'Photon-Stream for FACT\n'
+    out += '----------------------\n'
+    out += '\n'
+    out += 'cuts\n'
+    out += '----\n'
+    out += '- only observation runs (fRunTypeKey == 1)\n'
+    out += '- only expected trigger types: [physics, pedestal, ext1, ext2]\n'
+    out += '- expected trigger intensity < 300s * '+str(max_trigger_rate)+'Hz\n'
+    out += '- only runs with at least 1 expected trigger\n'
+    out += '\n'
+    out += '    from '+str(info['fNight'].min())+' to '+str(info['fNight'].max())+'\n'
+
+    out += '    '+table_header_str()
+    out += '    '+table_line_str()
+    out += '    '+table_row_str(
+        actual_events=total_actual_events, 
+        expected_events=total_expected_events)
+    out += '\n'
+
+    # full overview
+    out += '    by year\n'
+    first_year = tools.night_id_2_yyyy(info['fNight'].min())
+    last_year = tools.night_id_2_yyyy(info['fNight'].max())
+    out += '    year  '+table_header_str()
+    out += '    ------'+table_line_str()
+    for i in range(last_year - first_year + 1):
+        year = first_year + i
+        after_start_of_year = info['fNight'] > year*100*100
+        before_end_of_year = info['fNight'] < (year+1)*100*100
+        is_in_year =  after_start_of_year&before_end_of_year
+
+        expected_triggers_in_year = int((
+            info['fNumExt1Trigger'][valid&is_in_year] + 
+            info['fNumExt2Trigger'][valid&is_in_year] + 
+            info['fNumPhysicsTrigger'][valid&is_in_year] + 
+            info['fNumPedestalTrigger'][valid&is_in_year]).sum())
+        actual_triggers_in_year = int(info['photon_stream_NumTrigger'][valid&is_in_year].sum())
+        out += '    {year:04d}'.format(year=year)
+        out += '  '+table_row_str(
+            expected_events=expected_triggers_in_year, 
+            actual_events=actual_triggers_in_year)
+    out += '\n'
+    return out
+
+
+def table_header_str():
+    #       0        1         2         3         4          5         6         7    
+    #       12345678901234567890123456789012345678901234567898012345678901234567890
+    out =  'photon-stream events [#]  recorded events [#]  ratio [%]\n'
+    return out 
+
+
+def table_line_str():
+    out =  '--------------------------------------------------------\n'
+    return out
+
+
+def table_row_str(actual_events, expected_events):
+    out =  '{actual_events:>24d}  {expected_events:>19d}'.format(
+        actual_events=actual_events,
+        expected_events=expected_events)+'  '+progress(actual_events/expected_events)+'\n'
+    return out    
+
 
 def progress(ratio, length=20):
+    """
+    Returns a string with a progress bar e.g. '|||||| 55%'.
+
+    Parameters
+    ----------
+    ratio       A floating point number between 0.0 and 1.0.
+
+    length      The maximum length of the progress bar '||||' part.
+    """
     try:
         prog = int(np.round(ratio*length))
         percent = np.round(ratio*100)
