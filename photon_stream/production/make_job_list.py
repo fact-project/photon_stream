e5d23b2483f002f9b06b7e32f15d8d3ff47c63e3
sebastian <sebmuell@phys.ethz.ch>
2017-07-05 11:04:03 +0200

    do not relay on fact dir structure, enable manual assigmant of dirs

--- photon_stream/production/make_job_list.py
+++ photon_stream/production/make_job_list.py
@@ -1,32 +1,39 @@
 import os
+from os.path import abspath
 from os.path import join
 from os.path import exists
 from tqdm import tqdm
 import subprocess as sp
 import shutil
 import datetime as dt
+import pkg_resources
 
 from .runinfo import get_runinfo
 from . import tools
 from .write_worker_script import write_worker_script
 from .runinfo import observation_runs_in_runinfo_in_night_range
 from .runinfo import add_drs_run_info_to_jobs
-from .dummy_qsub import dummy_qsub
 
-def submit_to_qsub(
-    out_dir, 
-    start_night=20110101, 
+FACT_TOOLS_OBSERVATIONS_STEERING_CARD_PATH = pkg_resources.resource_filename(
+    'photon_stream',
+    os.path.join('production','observations_pass4.xml')
+)
+
+def make_job_list(
+    out_dir,
+    start_night=20110101,
     end_night=20501231,
     only_a_fraction=1.0,
-    fact_dir='/fact/', 
+    fact_raw_dir='/fact/raw',
+    fact_drs_dir='/fact/raw',
+    fact_aux_dir='/fact/aux',
     java_path='/home/guest/relleums/java8/jdk1.8.0_111',
     fact_tools_jar_path='/home/guest/relleums/fact_photon_stream/fact-tools/target/fact-tools-0.18.0.jar',
     fact_tools_xml_path='/home/guest/relleums/fact_photon_stream/photon_stream/photon_stream/production/observations_pass4.xml',
     tmp_dir_base_name='fact_photon_stream_JOB_ID_',
-    queue='fact_medium', 
-    email='sebmuell@phys.ethz.ch',
-    use_dummy_qsub=False,
-    runinfo=None):
+    runinfo=None,
+    only_append=True,
+):
     """
     Submitts jobs into qsub to process the FACT raw observations to the novel
     photon-stream representation.
@@ -47,7 +54,11 @@ def submit_to_qsub(
                         random fraction of the runs. Usefull for debugging over 
                         long periodes of observations. (default 1.0)
 
-    fact_dir            The path to the FACT files. At ISDC this is '/fact/'.
+    fact_raw_dir        The path to the FACT raw observation directory.
+
+    fact_drs_dir        The path to the FACT drs calibration directory.
+
+    fact_aux_dir        The path to the FACT auxiliary directory.
 
     java_path           The path to the JAVA run time environment to be used for
                         fact-tools.
@@ -59,41 +70,52 @@ def submit_to_qsub(
     tmp_dir_base_name   The base name of the temporary directory on the qsub 
                         worker nodes. (default 'fact_photon_stream_JOB_ID_')
 
-    queue               The name of the queue to submitt to. 
-                        (default 'fact_medium')
-    
-    email               The email address of the poor guy who is running the 
-                        conversion. (default 'sebmuell@phys.ethz.ch')
-
-    use_dummy_qsub      Uses not the real qsub but just a dummy which creates 
-                        dummy output files. For unit testing. (default False)
-
     runinfo             A pandas DataFrame() of the FACT run-info-database which
                         is used as a reference for the runs to be processed.
                         (default None, download the latest run-info on the fly)
+
+    only_append         If True, no output directories are created, but it is 
+                        asserted that there already output directories.
     """
     
     print('Start photon stream conversion ...')
 
     out_dir = os.path.abspath(out_dir)
-    fact_dir = os.path.abspath(fact_dir)
+    fact_raw_dir = abspath(fact_raw_dir)
+    fact_drs_dir = abspath(fact_drs_dir)
+    fact_aux_dir = abspath(fact_aux_dir)
     
     res_dir = join(out_dir, 'resources')
-    this_processing_resource_dir = join(res_dir, dt.datetime.utcnow().strftime('%Y%m%d_%Hh%Mm%Ss_%fus_UTC'))
+    this_processing_resource_dir = join(
+        res_dir, 
+        dt.datetime.utcnow().strftime('%Y%m%d_%Hh%Mm%Ss_%fus_UTC')
+    )
     std_dir = join(out_dir, 'std')
     job_dir = join(out_dir, 'job')
     phs_dir = join(out_dir, 'phs')
 
-    os.makedirs(out_dir, exist_ok=True)
-    os.makedirs(std_dir, exist_ok=True)
-    os.makedirs(job_dir, exist_ok=True)
-    os.makedirs(phs_dir, exist_ok=True)
-    os.makedirs(res_dir, exist_ok=True)
+    if only_append:
+        assert exists(out_dir)
+        assert exists(std_dir)
+        assert exists(job_dir)
+        assert exists(phs_dir)
+        assert exists(res_dir)
+    else:
+        os.makedirs(out_dir, exist_ok=True)
+        os.makedirs(std_dir, exist_ok=True)
+        os.makedirs(job_dir, exist_ok=True)
+        os.makedirs(phs_dir, exist_ok=True)
+        os.makedirs(res_dir, exist_ok=True)
+        
     os.makedirs(this_processing_resource_dir, exist_ok=False)
 
     print('Copy resources ...')
-    fact_tools_jar_path = os.path.abspath(shutil.copy(fact_tools_jar_path, this_processing_resource_dir))
-    fact_tools_xml_path = os.path.abspath(shutil.copy(fact_tools_xml_path, this_processing_resource_dir))
+    fact_tools_jar_path = os.path.abspath(
+        shutil.copy(fact_tools_jar_path, this_processing_resource_dir)
+    )
+    fact_tools_xml_path = os.path.abspath(
+        shutil.copy(fact_tools_xml_path, this_processing_resource_dir)
+    )
 
     if runinfo is None:
         runinfo = get_runinfo()
@@ -104,85 +126,62 @@ def submit_to_qsub(
         runinfo=runinfo,
         start_night=start_night, 
         end_night=end_night,
-        only_a_fraction=only_a_fraction)
+        only_a_fraction=only_a_fraction
+    )
 
     print('Found '+str(len(jobs))+' runs in database.')
-    print('Find overlap with runs accessible in "'+fact_dir+'" ...')
+    print('Find overlap with runs accessible in "'+fact_raw_dir+'" ...')
 
     for job in jobs:
         job['yyyy'] = tools.night_id_2_yyyy(job['Night'])
         job['mm'] = tools.night_id_2_mm(job['Night'])
         job['nn'] = tools.night_id_2_nn(job['Night'])
-        job['fact_dir'] = fact_dir
+        job['fact_raw_dir'] = fact_raw_dir
+        job['fact_drs_dir'] = fact_drs_dir
+        job['fact_aux_dir'] = fact_aux_dir
         job['yyyymmnn_dir'] = '{y:04d}/{m:02d}/{n:02d}/'.format(
             y=job['yyyy'],
             m=job['mm'],
-            n=job['nn'])
+            n=job['nn']
+        )
         job['base_name'] = '{bsn:08d}_{rrr:03d}'.format(
             bsn=job['Night'],
-            rrr=job['Run'])
+            rrr=job['Run']
+        )
         job['raw_file_name'] = job['base_name']+'.fits.fz'
         job['raw_path'] = join(
-            job['fact_dir'], 
-            'raw', 
+            job['fact_raw_dir'], 
             job['yyyymmnn_dir'], 
-            job['raw_file_name'])
+            job['raw_file_name']
+        )
         job['aux_dir'] = join(
-            job['fact_dir'], 
-            'aux', 
-            job['yyyymmnn_dir'])
+            job['fact_aux_dir'], 
+            job['yyyymmnn_dir']
+        )
 
     jobs = tools.jobs_where_path_exists(jobs, path='raw_path')
 
-    print('Found '+str(len(jobs))+' runs both in database and accesible in "'+fact_dir+'".')
+    print('Found '+str(len(jobs))+' runs both in database and accesible in "'+fact_raw_dir+'".')
     print('Find matching drs calibration runs ...')
 
     jobs = add_drs_run_info_to_jobs(runinfo=runinfo, jobs=jobs)
     jobs = tools.jobs_where_path_exists(jobs=jobs, path='drs_path')
 
     print('Found '+str(len(jobs))+' with accesible drs files.')
-    print('Submitt jobs into qsub ...')
 
-    for job in tqdm(jobs): 
+    for job in jobs: 
         job['std_dir'] = std_dir
         job['stdout_path'] = join(std_dir, job['base_name']+'.o')
         job['stderr_path'] = join(std_dir, job['base_name']+'.e')
         
         job['job_path'] = join(job_dir, 'PhotonStream_'+job['base_name']+'.sh')
         job['worker_tmp_dir_base_name'] = tmp_dir_base_name
-        job['email'] = email
-        job['queue'] = queue
 
         job['java_path'] = java_path
         job['fact_tools_jar_path'] = fact_tools_jar_path
         job['fact_tools_xml_path'] = fact_tools_xml_path
 
         job['phs_dir'] = join(phs_dir, job['yyyymmnn_dir'])
-        write_worker_script(
-            path=job['job_path'],
-            java_path=job['java_path'],
-            fact_tools_jar_path=job['fact_tools_jar_path'],
-            fact_tools_xml_path=job['fact_tools_xml_path'],
-            in_run_path=job['raw_path'],
-            drs_path=job['drs_path'],
-            aux_dir=job['aux_dir'],
-            out_dir=job['phs_dir'],
-            out_base_name=job['base_name'],
-            tmp_dir_base_name=job['worker_tmp_dir_base_name'],)
-
-        cmd = [ 'qsub',
-                '-q', queue,
-                '-o', job['stdout_path'],
-                '-e', job['stderr_path'],
-                '-m', 'ae', # send email in case of (e)nd or (a)bort
-                '-M', email,
-                job['job_path']]
-   
-        if use_dummy_qsub:
-            dummy_qsub(cmd)
-        else:
-            qsub_return_code = sp.call(cmd)
-            if qsub_return_code > 0:
-                print('qsub return code: ', qsub_return_code)
 
     print('Done.')
+    return jobs
