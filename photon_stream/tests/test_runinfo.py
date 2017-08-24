8656d59cb13082e39fafea859e86c6bf6c7735c2
sebastian <sebmuell@phys.ethz.ch>
2017-08-24 13:00:10 +0200

    test append to known_runs_cache

--- photon_stream/tests/test_runinfo.py
+++ photon_stream/tests/test_runinfo.py
@@ -1,6 +1,60 @@
 import numpy as np
 import photon_stream as ps
+import pkg_resources
+
+
+known_runs_path = pkg_resources.resource_filename(
+    'photon_stream', 
+    'tests/resources/known_runs_20110101.msg'
+)
+
+runinfo_path = pkg_resources.resource_filename(
+    'photon_stream', 
+    'tests/resources/runinfo_20120201.msg'
+)
+
+
+def test_known_runs_20110101_has_correct_keys():
+    known_runs = ps.production.runinfo.read_runinfo_from_file(known_runs_path)
+    for key in known_runs.keys():
+        assert key in (
+            ps.production.runinfo.ID_RUNINFO_KEYS + 
+            ps.production.runinfo.TRIGGER_NUMBER_RUNINFO_KEYS + 
+            ps.production.runinfo.PHS_RUNINFO_KEYS
+        )
+
+
+def test_runinfo_20120201_has_correct_keys():
+    runinfo = ps.production.runinfo.read_runinfo_from_file(runinfo_path)
+    for key in runinfo.keys():
+        assert key in (
+            ps.production.runinfo.ID_RUNINFO_KEYS + 
+            ps.production.runinfo.TRIGGER_NUMBER_RUNINFO_KEYS
+        )
 
 
 def test_append_runinfo():
-    pass
+    known_runs = ps.production.runinfo.read_runinfo_from_file(known_runs_path)
+    runinfo = ps.production.runinfo.read_runinfo_from_file(runinfo_path)
+
+    new_known_runs = ps.production.runinfo.append_runinfo_to_known_runs(
+        runinfo=runinfo,
+        known_runs=known_runs,
+    )
+
+    runs_in_fresh_runinfo = runinfo.shape[0]
+    columns_in_known_runs = known_runs.shape[1]
+
+    assert new_known_runs.shape[0] == runs_in_fresh_runinfo
+    assert new_known_runs.shape[1] == columns_in_known_runs
+
+    for i, row in new_known_runs.iterrows():
+        if i < known_runs.shape[0]:
+            for id_key in ps.production.runinfo.ID_RUNINFO_KEYS:
+                assert new_known_runs[id_key][i] == known_runs[id_key][i]
+
+            for phs_key in ps.production.runinfo.PHS_RUNINFO_KEYS:
+                assert new_known_runs[phs_key][i] == known_runs[phs_key][i]
+        else:
+            for phs_key in ps.production.runinfo.PHS_RUNINFO_KEYS:
+                assert new_known_runs[phs_key][i] == 0
