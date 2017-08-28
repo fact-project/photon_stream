02eb12bfd0ca7f8053672b636aa740be20b5b417
sebastian <sebmuell@phys.ethz.ch>
2017-08-28 23:19:44 +0200

    Use docopt to parse arguments

--- photon_stream/experimental/cpp_input_output/ExampleReader.c
+++ photon_stream/experimental/cpp_input_output/ExampleReader.c
@@ -1,5 +1,5 @@
 // compile using:
-// g++ ExampleReader.c -o PhotonStreamTest -lm
+// g++ docopt/docopt.cpp ExampleReader.c -o PhotonStreamTest -lm -std=gnu++11
 // 
 // gunzip the example binary run in the photon_stream/tests/resources and
 // run the executable right next tu the unzipped binary file.
@@ -8,16 +8,57 @@
 #include <stdio.h>
 #include <string.h>
 #include "photon_stream.h"
+#include "docopt/docopt.h"
 #include <fstream>
 #include <iostream>
 
 namespace ps = photon_stream;
 
-int main() {
+static const char USAGE[] =
+R"(Show FACT event overview
 
-    std::string path = "20170119_229_pass4_100events.phs.bin";
+    Usage:
+      PhsShow --input=PATH [--trigger=TYPE]
+      PhsShow (-h | --help)
+      PhsShow --version
+
+    Options:
+      -i --input=PATH     Input event list path.
+      -t --trigger=TYPE   Only show certain trigger type.
+      -h --help           Show this screen.
+      --version           Show version.
+      
+)";
+
+
+void print_event_info_line(ps::ObservationEvent &event) {
+    printf(
+        "%05d %03d %5d %6d  %3.2f  %3.2f  %10.6f  %6d\n",  
+        event.id.night,
+        event.id.run,
+        event.id.event,
+        event.info.trigger_type,
+        event.pointing.az,
+        event.pointing.zd,
+        float(event.info.unix_time_s) + 1e-6*float(event.info.unix_time_us),
+        event.photon_stream.number_of_photons()
+    );
+};
+
+int main(int argc, char* argv[]) {
+
+    std::map<std::string, docopt::value> args = docopt::docopt(
+        USAGE,
+        { argv + 1, argv + argc },
+        true,        // show help if requested
+        "mct 0.0"
+    );  // version string
+
+    std::string path = args.find("--input")->second.asString();
     std::ifstream fin(path.c_str(), std::ios::in | std::ios::binary);
 
+    printf("night    run event trigger  Az[deg] ZD[deg]  UnixTime[s]   photons\n");
+    printf("------------------------------------------------------------------\n");
     if (fin.is_open()) {
         while(true) {
             ps::Descriptor desc = ps::read_Descriptor_from_file(fin);
@@ -25,13 +66,19 @@ int main() {
                 break;
 
             ps::ObservationEvent event = ps::read_ObservationEvent_from_file(fin);
-            std::cout << "Event(night:" << event.id.night << ", ";
-            std::cout << "run:" << event.id.run << ", ";
-            std::cout << "event:" << event.id.event << ") ";
-            std::cout << "photons:" << event.photon_stream.number_of_photons() << "\n";
-    }
 
-    }else {
+            if(args.find("--trigger")->second) {
+                if(
+                    std::stoi(args.find("--trigger")->second.asString()) == 
+                    event.info.trigger_type
+                ) {
+                    print_event_info_line(event);
+                }
+            }else{
+                print_event_info_line(event);     
+            }
+        }
+    }else{
         std::cout << "Error opening file:" << path << "\n";
     }
 
