4ea406f5dc5b06754c2bd996701a3da660303d2f
sebastian <sebmuell@phys.ethz.ch>
2017-08-30 17:27:20 +0200

    A viewer to go through the time slices

--- photon_stream/experimental/cpp_input_output/example_viewer.cpp
+++ photon_stream/experimental/cpp_input_output/example_viewer.cpp
@@ -1,5 +1,5 @@
 // compile using:
-// g++ docopt/docopt.cpp example_viewer.cpp -o phs.view -std=gnu++11
+// g++ docopt/docopt.cpp example_viewer.cpp -o phs.slices -std=gnu++11
 
 #include <stdio.h>
 #include <string.h>
@@ -14,14 +14,15 @@ namespace ps = photon_stream;
 
 static const char USAGE[] =
 R"(Show FACT photon-stream event overview. Reads in phs
-files from stdin. 
+files from ungzipped file. 
 
     Usage:
-      phs.view
-      phs.view  (-h | --help)
-      phs.view  --version
+      phs.slices  --input=PATH
+      phs.slices  (-h | --help)
+      phs.slices  --version
 
     Options:
+      -i --input=PATH     Path to the ungzipped photon-stream file.
       -h --help           Show this screen.
       --version           Show version.
       
@@ -46,17 +47,76 @@ std::array<int, 9> nx =   { 1, 0,-1, 1, 0,-1, 1, 0,-1};
 std::array<int, 9> ny =   { 1, 1, 1, 0, 0, 0,-1,-1,-1};
 std::array<float, 9> we = {.3,.5,.3,.5, 1,.5,.3,.5,.3};
 
-void print_image(ps::ObservationEvent &event) {
-    std::array<uint64_t, ps::NUMBER_OF_PIXELS> img = ps::image_integral(
-        event.photon_stream
-    );
+const uint64_t NUMBER_OF_TIME_INTEGRAL_SLICES = 19;
+# define cmdl_image std::array<std::array<float, 80>, 80>
+# define cmdl_image_seq std::array<cmdl_image, NUMBER_OF_TIME_INTEGRAL_SLICES>
+# define phs_integral std::array<uint64_t, ps::NUMBER_OF_PIXELS>
+# define phs_lol std::array<std::vector<uint8_t>, ps::NUMBER_OF_PIXELS>
 
-    std::array<std::array<float, 80>, 80> bg;
+
+cmdl_image normalized_cmdl_image(cmdl_image in, float max) {
+    cmdl_image out;
+    for(int x=0; x<in.size(); x++) {
+        for(int y=0; y<in.at(x).size(); y++) {
+            out.at(x).at(y) = in.at(x).at(y) / max;
+        }
+    }
+    return out;
+}
+
+
+void print_cmdl_image(cmdl_image img, float max) {
+
+    img = normalized_cmdl_image(img, max);
+
+    std::array<std::array<uint8_t, 80>, 80> color_code_img;
+    for(int x=0; x<img.size(); x++) {
+        for(int y=0; y<img.at(x).size(); y++) {
+            color_code_img.at(x).at(y) = intensity_to_color_code(
+                img.at(x).at(y)
+            );
+        }
+    }
+
+    std::stringstream out;
+    for(int x=0; x<color_code_img.size(); x++) {
+        for(int y=0; y<color_code_img.at(x).size(); y++) {
+            uint8_t color_code = color_code_img.at(x).at(y);
+            std::string color = std::to_string(color_code);
+            out << "\033[48;5;" << color << "m  \033[0m";
+        }
+        out << "\n";
+    }
+
+    std::cout << out.str();
+}
+
+
+void print_colorbar(float min, float max) {
+    std::stringstream out;
+    std::array<uint8_t, 65> bar;
+    for(int i=0; i<bar.size(); i++) {
+        float u = float(i)/bar.size();
+        bar.at(i) = intensity_to_color_code(u);
+    }
+    out << min << " [";
+    for(int i=0; i<bar.size(); i++) {
+        uint8_t color_code = bar.at(i);
+        std::string color = std::to_string(color_code);
+        out << "\033[48;5;" << color << "m  \033[0m";
+    }
+    out << "] " << max << " photons\n";
+    std::cout << out.str();
+}
+
+
+cmdl_image phs_integral_to_cmdl_image(phs_integral integral) {
+    cmdl_image bg;
     for(int x=0; x<bg.size(); x++)
         for(int y=0; y<bg.at(x).size(); y++)
             bg.at(x).at(y) = 0;
 
-    for(int chid=0; chid<img.size(); chid++) {
+    for(int chid=0; chid<integral.size(); chid++) {
         float x = px[chid]/5.0 + 2*19.0;
         float y = py[chid]/5.0 + 2*19.0;
 
@@ -68,66 +128,84 @@ void print_image(ps::ObservationEvent &event) {
             int ay = cy + ny.at(n);
 
             if(ax>=0 && ax<80 && ay>=0 && ay<80) {
-                bg.at(ax).at(ay) += we.at(n)*float(img.at(chid));
+                bg.at(ax).at(ay) += we.at(n)*float(integral.at(chid));
             }
         }
     }
-
-    float Mmax = 0.0;
-    for(int x=0; x<bg.size(); x++) {
-        for(int y=0; y<bg.at(x).size(); y++) {
-            if(bg.at(x).at(y) > Mmax)
-                Mmax = bg.at(x).at(y);
-        }
-    }
-
-    std::array<std::array<float, 80>, 80> normal;
-    for(int x=0; x<bg.size(); x++) {
-        for(int y=0; y<bg.at(x).size(); y++) {
-            normal.at(x).at(y) = float(bg.at(x).at(y))/Mmax;
-        }
-    }
-
-    std::array<std::array<uint8_t, 80>, 80> color_code_img;
-    for(int x=0; x<bg.size(); x++) {
-        for(int y=0; y<bg.at(x).size(); y++) {
-            color_code_img.at(x).at(y) = intensity_to_color_code(
-                normal.at(x).at(y)
-            );
-        }
-    }
-
-    std::stringstream out;
-    for(int x=0; x<bg.size(); x++) {
-        for(int y=0; y<bg.at(x).size(); y++) {
-            uint8_t color_code = color_code_img.at(x).at(y);
-            std::string color = std::to_string(color_code);
-            out << "\033[48;5;" << color << "m  \033[0m";
-        }
-        out << "\n";
-    }
-
-    // colorbar
-    std::array<uint8_t, 65> bar;
-    for(int i=0; i<bar.size(); i++) {
-        float u = float(i)/bar.size();
-        bar.at(i) = intensity_to_color_code(u);
-    }
-
-    out << "\n";
-    out << "0 [";
-    for(int i=0; i<bar.size(); i++) {
-        uint8_t color_code = bar.at(i);
-        std::string color = std::to_string(color_code);
-        out << "\033[48;5;" << color << "m  \033[0m";
-    }
-    out << "] " << Mmax << " photons\n";
-    std::cout << out.str();
+    return bg;
 }
+
+
+phs_integral phs_integral_start_stop(phs_lol& phs, uint8_t start, uint8_t stop) {
+    phs_integral integral;
+    for(uint64_t chid=0; chid<integral.size(); chid++)
+        integral.at(chid) = 0;
+
+    for(uint64_t chid=0; chid<phs.size(); chid++) {
+        for(uint64_t ph=0; ph<phs.at(chid).size(); ph++) {
+            uint8_t t = phs.at(chid).at(ph);
+            if(t >= start && t < stop)
+                integral.at(chid) += 1;
+        }
+    }
+    return integral;
+}
+
+
+float max_intensity_in_cmdl_image(cmdl_image img) {
+    float max = 0.0;
+    for(int x=0; x<img.size(); x++) {
+        for(int y=0; y<img.at(x).size(); y++) {
+            if(img.at(x).at(y) > max)
+                max = img.at(x).at(y);
+        }
+    }
+    return max;
+}
+
+
+struct ArgMax {
+    float max;
+    uint64_t arg;
+};  
+
+
+ArgMax argmax_intensity_in_cmdl_image_seq(cmdl_image_seq img_seq) {
+    ArgMax argmax;
+    argmax.max = 0;
+    for(uint64_t i=0; i<img_seq.size(); i++) {
+        float maxi = max_intensity_in_cmdl_image(img_seq.at(i));
+        if(maxi > argmax.max) {
+            argmax.max = maxi;
+            argmax.arg = i;
+        }            
+    }
+    return argmax;
+}
+
+
+struct Slices{
+    ArgMax arg_max;
+    cmdl_image_seq img_seq;
+};
+
+
+Slices make_slices_from_phs_lol(phs_lol lol) {
+    Slices slices;
+    for(uint64_t i=0; i<slices.img_seq.size(); i++) {
+        uint8_t start = i*5 + ps::NUMBER_OF_TIME_SLICES_OFFSET_AFTER_BEGIN_OF_ROI;
+        uint8_t stop = start + 10 + ps::NUMBER_OF_TIME_SLICES_OFFSET_AFTER_BEGIN_OF_ROI;
+        phs_integral phs_i = phs_integral_start_stop(lol, start, stop);
+        slices.img_seq.at(i) = phs_integral_to_cmdl_image(phs_i);
+    }
+    slices.arg_max = argmax_intensity_in_cmdl_image_seq(slices.img_seq);
+    return slices;
+}
+
 
 void print_event_info_line(ps::ObservationEvent &event) {
     printf(
-        "Night %05d, Run %03d, Event %5d, Trigger %6d, Az %3.2f, Zd %3.2f, UnixTime %10.6f, photons %6d\n",  
+        "Night %5d  Run %3d  Event %6d  Trigger %6d\nAzimuth %3.2fdeg  Zenith distance %3.2fdeg\nUnixTime %10.6fs  photons %6d\n",  
         event.id.night,
         event.id.run,
         event.id.event,
@@ -139,6 +217,82 @@ void print_event_info_line(ps::ObservationEvent &event) {
     );
 };
 
+
+void print_time_slice_bar(int slice) {
+    int start_slice = slice*5;
+    int end_slice = slice*5+10;
+    int END = NUMBER_OF_TIME_INTEGRAL_SLICES*5 + 5;
+    std::cout << "Time              <";
+    std::cout << std::string(start_slice, '.');
+    std::cout << "[        ]";
+    std::cout << std::string(END - end_slice, '.');
+    std::cout << "> ";
+    std::cout << 0.5*float(start_slice + ps::NUMBER_OF_TIME_SLICES_OFFSET_AFTER_BEGIN_OF_ROI) << "ns to ";
+    std::cout << 0.5*float(end_slice + ps::NUMBER_OF_TIME_SLICES_OFFSET_AFTER_BEGIN_OF_ROI) << "ns\n";
+}
+
+
+std::vector<ps::ObservationEvent> read_in_event_list(std::string path) {
+	std::vector<ps::ObservationEvent> events;
+	std::ifstream fin(path.c_str(), std::ios::binary);
+    while(true) {
+        ps::ObservationEvent event = ps::read_ObservationEvent_from_file(fin);
+        if(!event.descriptor.is_valid())
+            break;
+        events.push_back(event);
+    }
+    fin.close();
+    return events;
+}
+
+const char ESCAPE_KEY = 27;
+
+int event_idx = 0;
+int slice_idx = 0;
+std::vector<ps::ObservationEvent> events;
+
+void next_event_if_possible() {
+    if(event_idx + 1 < events.size())
+        event_idx++;
+};
+
+void prev_event_if_possible() {
+    if(event_idx - 1 >= 0)
+        event_idx--;
+};
+
+void next_slice_if_possible() {
+    if(slice_idx + 1 < NUMBER_OF_TIME_INTEGRAL_SLICES)
+        slice_idx++;
+};
+
+void prev_slice_if_possible() {
+    if(slice_idx - 1 >= 0)
+        slice_idx--;
+};
+
+void go_to_event_id(int event_id) {
+    for(int i=0; i<events.size(); i++) {
+        if(events.at(i).id.event == event_id)
+            event_idx = i;
+    }
+}
+
+void print_help() {
+    std::cout << "Help\n";
+    std::cout << "----\n";
+    std::cout << "  [h]  print this help\n";
+    std::cout << "\n";
+    std::cout << "  [a]  next event\n";
+    std::cout << "  [p]  previous event\n";
+    std::cout << "\n";   
+    std::cout << "  [g]  go to event ID if existing\n";
+    std::cout << "\n";
+    std::cout << "  [w]  next time slice integral\n";
+    std::cout << "  [s]  previous time slice integral\n";
+}
+
+
 int main(int argc, char* argv[]) {
 
     std::map<std::string, docopt::value> args = docopt::docopt(
@@ -148,14 +302,79 @@ int main(int argc, char* argv[]) {
         "mct 0.0"
     );  // version string
 
-    while(true) {
-        ps::ObservationEvent event = ps::read_ObservationEvent_from_file(std::cin);
+    std::string path = args.find("--input")->second.asString();
+    events = read_in_event_list(path);
 
-        if(!event.descriptor.is_valid())
-            break;
+    if(events.size() == 0)
+        return 0;
 
-        print_event_info_line(event);
-        print_image(event);
+    ps::ObservationEvent current_event = events.at(0);
+    Slices current_slices = make_slices_from_phs_lol(
+        ps::list_of_lists_representation(current_event.photon_stream)
+    );
+    slice_idx = current_slices.arg_max.arg;
+
+    bool go_on = true;
+    bool update_event = true;
+    bool update_slice = false;
+
+    print_help();
+    while(go_on) {
+        std::string user_input;
+        std::getline(std::cin, user_input);
+        
+        update_event = true;
+        update_slice = false;
+        if(user_input.length() > 0) {
+            char first_key = user_input.at(0);
+            if(first_key == ESCAPE_KEY) {
+                go_on = false;
+                update_event = false;
+            }else if(first_key == 'd') {
+                next_event_if_possible();
+            }else if(first_key == 'a') {
+                prev_event_if_possible();
+            }else if(first_key == 'w') {
+                next_slice_if_possible();
+                update_event = false;
+                update_slice = true;
+            }else if(first_key == 's') {
+                prev_slice_if_possible();
+                update_event = false;
+                update_slice = true;
+            }else if(first_key == 'g') {
+                std::string event_id_str = user_input.substr(1);
+                go_to_event_id(atoi(event_id_str.c_str()));
+            }else if(first_key == 'h') {
+                print_help();
+                update_event = false;
+            }else{
+                update_event = false;
+            }
+
+        }else{
+            update_event = false;
+        }
+
+        if(update_event) {
+            current_event = events.at(event_idx);
+            current_slices = make_slices_from_phs_lol(
+                ps::list_of_lists_representation(current_event.photon_stream)
+            );
+            slice_idx = current_slices.arg_max.arg;
+        }
+
+        if(update_event || update_slice) {
+            std::cout << std::string(120, '\n');
+            cmdl_image img = current_slices.img_seq.at(slice_idx);
+            print_cmdl_image(img, current_slices.arg_max.max);
+            print_colorbar(0.0, current_slices.arg_max.max);
+            std::cout << "\n";
+            print_time_slice_bar(slice_idx);
+            std::cout << "\n";
+            print_event_info_line(current_event);
+        }   
     }
+
     return 0;
 };
