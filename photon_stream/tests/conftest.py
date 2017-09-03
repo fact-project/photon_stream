d022650a14985c5035248a8c19b3d7204330b9d6
sebastian <sebmuell@phys.ethz.ch>
2017-09-03 18:34:44 +0200

    out_dir doc

--- photon_stream/tests/conftest.py
+++ photon_stream/tests/conftest.py
@@ -4,13 +4,14 @@ def pytest_addoption(parser):
     parser.addoption(
         "--out_dir", 
         action="store", 
-        default=None,
-        help=("Writes the photon-stream obs production output into a permament "
-              "directory. This contains the fake fact/raw file tree. "
-              "Default is temporary directory."
-        )
+        default=None
     )
 
 @pytest.fixture
 def out_dir(request):
+    """
+    Writes the photon-stream obs production output into a permament directory. 
+    This contains the fake fact/raw file tree.
+    Default is temporary directory.
+    """
     return request.config.getoption("--out_dir")
