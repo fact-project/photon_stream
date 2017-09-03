dc58dac3a30f2b438baa852110f81c11532c22c3
sebastian <sebmuell@phys.ethz.ch>
2017-09-03 14:16:39 +0200

    use pyfact.path for night and run to string, backup feature to complicated

--- photon_stream/production/tools.py
+++ photon_stream/production/tools.py
@@ -1,29 +1,4 @@
-import datetime as dt
 import os
-import numpy as np
-
-
-def night_id_2_yyyy(night):
-    return np.int(np.round(night)) // 10000
-
-
-def night_id_2_mm(night):
-    return (np.int(np.round(night)) // 100) % 100
-
-
-def night_id_2_nn(night):
-    return np.int(np.round(night)) % 100
-
-
-def time_stamp_utcnow_for_valid_path():
-	return dt.datetime.utcnow().strftime('%Y%m%d_%Hh%Mm%Ss_%fus_UTC')
-
-
-def local_backup_path_with_timestamp(path):
-    time_stamp = time_stamp_utcnow_for_valid_path()
-    basename = os.path.basename(path)
-    dirname = os.path.dirname(path)
-    return os.path.join(dirname, '.'+basename+'.'+time_stamp)
 
 
 def touch(path):
