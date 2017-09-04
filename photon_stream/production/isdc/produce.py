a88466c684ddd729e5d5d9653a2f3a6f269f2b2e
sebastian <sebmuell@phys.ethz.ch>
2017-09-04 21:13:34 +0200

    update doc

--- photon_stream/production/isdc/produce.py
+++ photon_stream/production/isdc/produce.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
 """
-Converting FACT raw observation runs into photon-stream runs. You need to export
-the universal FACT password: export FACT_PASSWORD=*********
+Convert another chunk of fact/raw/ observation runs into phs/obs/ runs. The 
+chunk size depends on the demand (output already exists) and wheter qstat is 
+busy. The maximum chunk size is 128 runs. 
+
+Export the FACT password: export FACT_PASSWORD=*********
 
 Usage: phs.isdc.obs.produce [options]
 
