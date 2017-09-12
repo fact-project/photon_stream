e61e32832803c3ac496fbcfe888d737013c14c47
sebastian <sebmuell@phys.ethz.ch>
2017-09-12 20:33:09 +0200

    remove verbose noise, no wait for input any longer

--- photon_stream/production/sim/extract_single_photons.py
+++ photon_stream/production/sim/extract_single_photons.py
@@ -11,6 +11,11 @@ fact_tools_xml_path = pkg_resources.resource_filename(
     os.path.join('production','resources','simulations_pass4.xml')
 )
 
+mc_drs_path = pkg_resources.resource_filename(
+    'photon_stream', 
+    os.path.join('production','resources','testMcDrsFile.drs.fits.gz')
+)
+
 
 def extract_single_photons(
     ceres_path,
@@ -18,8 +23,8 @@ def extract_single_photons(
     out_basename,
     java_path='/home/guest/relleums/java8/jdk1.8.0_111',
     fact_tools_jar_path='/home/guest/relleums/fact_photon_stream/fact-tools/target/fact-tools-0.18.1.jar',
-    mc_drs_file='/home/guest/relleums/fact_photon_stream/fact-tools/src/main/resources/testMcDrsFile.drs.fits.gz',
     fact_tools_xml_path=fact_tools_xml_path,
+    mc_drs_path=mc_drs_path,
     tmp_dir_prefix='phs_sim_produce_',
 ):
     with tempfile.TemporaryDirectory(prefix=tmp_dir_prefix) as tmp:
@@ -27,7 +32,7 @@ def extract_single_photons(
         my_env = os.environ.copy()
         my_env["PATH"] = java_path + my_env["PATH"]
 
-        cmd = [
+        subprocess.call([
             'java',
             '-XX:MaxHeapSize=1024m',
             '-XX:InitialHeapSize=512m',
@@ -37,13 +42,9 @@ def extract_single_photons(
             '-XX:+UseParNewGC',
             '-jar', fact_tools_jar_path, fact_tools_xml_path,
             '-Dinfile=file:'+ceres_path,
-            '-Ddrsfile=file:'+mc_drs_file,
+            '-Ddrsfile=file:'+mc_drs_path,
             '-Dout_path_basename='+join(tmp, out_basename),
-        ]
-        for k in cmd:
-            print(k)
-        input('wait')
-        subprocess.call(cmd)
+        ])
 
         for tmp_path in glob(join(tmp, '*')):
             if os.path.isfile(tmp_path):
