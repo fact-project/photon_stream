4a81937681a15ac48164a2acd31d90eb02478c68
sebastian <sebmuell@phys.ethz.ch>
2017-09-12 19:14:24 +0200

    read both gzipped and non gzipped corsika cer files

--- photon_stream/SimulationReader.py
+++ photon_stream/SimulationReader.py
@@ -1,5 +1,7 @@
 from .Event import Event
 from .EventListReader import EventListReader
+from .io import is_gzipped_file
+import gzip
 from .simulation_truth.corsika_headers import read_corsika_headers_from_file
 from .simulation_truth.corsika_headers import IDX_RUNH_RUN_NUMBER
 from .simulation_truth.corsika_headers import IDX_EVTH_EVENT_NUMBER
@@ -17,10 +19,7 @@ class SimulationReader(object):
     def __init__(self, photon_stream_path, mmcs_corsika_path):
         self.reader = EventListReader(photon_stream_path)
         self.mmcs_corsika_path = mmcs_corsika_path
-        with open(self.mmcs_corsika_path, 'rb') as fin:
-            mmcs_corsika_headers = read_corsika_headers_from_file(fin)
-        self.run_header = mmcs_corsika_headers['run_header']
-        self.event_headers = mmcs_corsika_headers['event_headers']
+        self._read_mmcs_corsika_headers()
         self.id_to_index = {}
         for idx in range(self.event_headers.shape[0]):
             event_id = self.event_headers[idx][IDX_EVTH_EVENT_NUMBER]
@@ -48,6 +47,18 @@ class SimulationReader(object):
         )
 
         return event
+
+
+    def _read_mmcs_corsika_headers(self):
+        if is_gzipped_file(self.mmcs_corsika_path):
+            with gzip.open(self.mmcs_corsika_path, 'rb') as fin:
+                headers = read_corsika_headers_from_file(fin)
+        else:
+            with open(self.mmcs_corsika_path, 'rb') as fin:
+                headers = read_corsika_headers_from_file(fin)
+        self.run_header = headers['run_header']
+        self.event_headers = headers['event_headers']
+
 
     def __repr__(self):
         out = '{}('.format(self.__class__.__name__)
