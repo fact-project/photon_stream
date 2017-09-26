582301ed79cef2992b8c42bf8942f98b607e9871
sebastian <sebmuell@phys.ethz.ch>
2017-09-26 11:36:08 +0200

    Expose the normalized photon-stream space and have a free function to do the normalization (space scaling between directions cx, cy and arrival time t)

--- photon_stream/PhotonCluster.py
+++ photon_stream/PhotonCluster.py
@@ -1,21 +1,30 @@
 from sklearn.cluster import DBSCAN
 import numpy as np
 
+
+def normalized_photon_stream_cloud_repr(photon_stream, deg_over_s=0.35e9):
+    xyt = photon_stream.flatten()
+    if xyt.shape[0] == 0:
+        return xyt
+    fov_radius = photon_stream.geometry['fov_radius']
+    xyt[:,0:2] /= (fov_radius*2.0)
+    xyt[:,2] /= (fov_radius*2.0)/deg_over_s
+    return xyt
+
+
 class PhotonStreamCluster(object):
     def __init__(self, photon_stream, eps=0.1, min_samples=20, deg_over_s=0.35e9):
+        self.xyt = normalized_photon_stream_cloud_repr(
+            photon_stream=photon_stream,
+            deg_over_s=deg_over_s,
+        )
 
-        xyt = photon_stream.flatten()
-
-        if xyt.shape[0] == 0:
+        if self.xyt.shape[0] == 0:
             self.labels = np.array([])
             self.number = 0
             return
-        fov_radius = photon_stream.geometry['fov_radius']
 
-        xyt[:,0:2] /= (fov_radius*2.0)
-        xyt[:,2] /= (fov_radius*2.0)/deg_over_s
-
-        dbscan = DBSCAN(eps=eps, min_samples=min_samples).fit(xyt)
+        dbscan = DBSCAN(eps=eps, min_samples=min_samples).fit(self.xyt)
         self.labels = dbscan.labels_
 
         # Number of clusters in labels, ignoring background if present.
