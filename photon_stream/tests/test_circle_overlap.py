974568ef6715677da24c002b342d79b97cd4315f
Laurits7 <laurits.tani@gmail.com>
2018-09-11 18:17:42 +0200

    triggering the warning

--- photon_stream/tests/test_circle_overlap.py
+++ photon_stream/tests/test_circle_overlap.py
@@ -28,4 +28,10 @@ def test_ring_overlapp():
         cx1=0.0, cy1=0.0, r1=100.0,
         cx2=0.0, cy2=101.0, r2=1.0)
     assert overlapp == 0.0
+    
 
+
+def test_trigger_warning():
+    overlapp = ps.muons.tools.circle_overlapp(
+        cx1=0.0, cy1=0.0, r1=1,
+        cx2=0.0, cy2=2, r2=2)   
